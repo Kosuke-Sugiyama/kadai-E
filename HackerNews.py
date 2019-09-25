f396a238e89dc4273cc85fe6439e2f79b48d9da5
kosuke <sheep.a08@gmail.com>
2019-09-25 14:25:42 +0900

    [課題F]WebAPIの利用の実装
    社内ネットでAPIを使用することができないため、
    動いていると仮定してプログラムを組む

---
+++ HackerNews.py
@@ -0,0 +1,24 @@
+import requests
+import time
+
+
+def main():
+    for i in range(3):
+        time.sleep(1)  # ここで1秒止まる
+
+    search_URL = 'https://hacker-news.firebaseio.com/v0/topstories.json'
+    r = requests.get(search_URL)
+    news_list = r[0:4]
+    # news_list = [21064942, 21067487, 21064911, 21062180, 21066385]
+
+    for news_id in news_list:
+        news_dict = {}
+        article_URL = f"https://hacker-news.firebaseio.com/v0/item/{news_id}.json?print=pretty"
+        request = requests.get(article_URL)
+        news_dict['title'] = request.json()["title"]
+        news_dict['url'] = request.json()["url"]
+        print(news_dict)
+
+
+if __name__ == '__main__':
+    main()
